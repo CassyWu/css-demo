8f8e2b763f5c21f2c02f70631cbd70def2b2cfe3
Cassy <wppdeyouxiang@163.com>
2020-02-09 11:04:48 +0800

    shadow animation for btn

--- src/button/index.js
+++ src/button/index.js
@@ -5,6 +5,7 @@ class Button extends Component{
         return(
             <div className="btn_wrapper">
                 <div className="btn-1"><span>Hover Me</span></div>
+                <div className="btn-2">Hover me</div>
             </div>
         );
     }
